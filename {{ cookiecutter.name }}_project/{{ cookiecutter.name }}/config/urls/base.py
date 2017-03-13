74bb6f9438939443a8cec571708708c946ac4b77
Alexander Temnikov <alexander.temnikov@me.com>
2017-03-13 14:09:29 +0300

    Add jet urls

--- {{ cookiecutter.name }}_project/{{ cookiecutter.name }}/config/urls/base.py
+++ {{ cookiecutter.name }}_project/{{ cookiecutter.name }}/config/urls/base.py
@@ -9,6 +9,16 @@ urlpatterns = [
         view=IndexView.as_view(),
         name='index'
     ),
+    {% if cookiecutter.use_jet_admin == 'y' %}
+    url(
+        regex=r'^jet/',
+        view=include('jet.urls', 'jet')
+    ),
+    url(
+        regex=r'^jet/dashboard/',
+        view=include('jet.dashboard.urls', 'jet-dashboard')
+    ),
+    {% endif %}
     url(
         regex=r'^admin/',
         view=admin.site.urls
