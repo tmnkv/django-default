0c3a34892eae6ce7fbe4bc43850e2afb4e255fcd
Alexander Temnikov <alexander.temnikov@me.com>
2017-03-13 15:27:14 +0300

    Add forms for header and footer admin

---
+++ {{ cookiecutter.name }}_project/{{ cookiecutter.name }}/apps/core/forms.py
@@ -0,0 +1,40 @@
+{% if cookiecutter.use_mptt == 'y' -%}
+from mptt.forms import TreeNodeChoiceField
+
+{%- endif %}
+from django.forms import ModelForm, Select
+{% if cookiecutter.use_mptt == 'y' -%}
+from apps.core.models import Menu, Page, Header, Footer
+
+
+class HeaderForm(ModelForm):
+    parent = TreeNodeChoiceField(
+        label='Родитель',
+        queryset=Header.objects.all(),
+        widget=Select(attrs={'class': 'selector'}),
+        required=False
+    )
+    page = TreeNodeChoiceField(
+        label='Страница',
+        queryset=Page.objects.all(),
+        widget=Select(attrs={'class': 'selector'}),
+        required=True
+    )
+
+    class Meta:
+        model = Header
+        fields = '__all__'
+
+
+class FooterForm(HeaderForm):
+    parent = TreeNodeChoiceField(
+        label='Родитель',
+        queryset=Footer.objects.all(),
+        widget=Select(attrs={'class': 'selector'}),
+        required=False
+    )
+
+    class Meta:
+        model = Footer
+        fields = '__all__'
+{%- endif %}
