27560230fc9d4ac0dcd0c5ff7d85ce81561588ba
Alexander Temnikov <alexander.temnikov@me.com>
2017-03-28 12:08:26 +0300

    Change webpack.config and package.json

--- {{ cookiecutter.name }}_project/{{ cookiecutter.name }}/webpack.config.js
+++ {{ cookiecutter.name }}_project/{{ cookiecutter.name }}/webpack.config.js
@@ -1,51 +1,81 @@
-var webpack = require("webpack");
-var autoprefixer = require('autoprefixer');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const path = require('path');
+const webpack = require("webpack");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    context: __dirname + "/static/src/",
-    entry: './index.js',
+    context: path.resolve(__dirname, 'static/src'),
+    entry: {
+        index: './index.js',
+    },
     output: {
-        path: __dirname + "/static/build/",
-        filename: "js/build.js"
+        path: path.resolve(__dirname, 'static/build'),
+        filename: 'js/[name].bundle.js',
+        publicPath: '/static/',
     },
     module: {
-        loaders: [
+        rules: [
             {
-              test: /\.(jpg|png|ico|gif|tif|svg)$/,
-              loader: 'url?name=img/[hash].[name].[ext]&limit=50000'
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [{loader: "babel-loader",
+                    options: { presets: ['es2015'] }
+                }],
+
             },
             {
-              test: /\.(woff|woff2|eot|ttf)$/,
-              loader: 'url?name=fonts/[hash].[name].[ext]'
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    //resolve-url-loader may be chained before sass-loader if necessary
+                    use: [
+                        'css-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: function () {
+                                    return [
+                                        require('autoprefixer')
+                                    ];
+                                }
+                            }
+                        },
+                        'sass-loader'
+                    ]
+                })
             },
             {
-                test: /\.(scss|css)$/,
-                loader: ExtractTextPlugin.extract("style", "css!postcss!sass")
-            }
+                test: /(\.jpg|\.png|\.ico|.gif|\.tif|\.svg)\??.*$/,
+                use: 'url-loader?limit=30000&name=images/[name].[ext]?v=[hash]'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf)$/,
+                loader: 'file-loader?name=fonts/[name].[ext]?v=[hash]'
+            },
         ]
     },
-    postcss: function () {
-        return [autoprefixer];
-    },
-
     plugins: [
-        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'commons',
+            filename: 'js/common.js',
+            minChunks: 2,
+        }),
+        new ExtractTextPlugin({
+            filename: 'css/styles.css',
+            allChunks: true,
+        }),
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
-            "window.jQuery": "jquery",
-            Cookies: "js-cookie"
-
+            "window.jQuery": "jquery"
         }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {warnings: false}
-        }),
-        new ExtractTextPlugin('css/build.css', {allChunks: true}),
-        new CopyWebpackPlugin([
-            {from: 'img/', to: 'img/'}
-        ])
-    ]
+    ],
+    devServer: {
+        proxy: {
+            "*": {
+                target: "http://localhost:8000",
+                changeOrigin: true,
+                secure: false,
+            }
+        }
+    }
 };
